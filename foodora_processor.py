8efa48b8c8a896f44409338bca0a725b2aae7b6c
Muadiv <info@muadiv.com.ar>
2023-12-27 19:33:11 +0100

    Working code ! ✅

--- foodora_processor.py
+++ foodora_processor.py
@@ -2,7 +2,6 @@ import pandas as pd
 import glob
 from constants import PRODUCT_LIST, PROVIDER_DICT
 
-# Mapping for Foodora locations
 FOODORA_LOCATION_MAP = {
     "Bistro La Paisanita": "Zizkov",
     "La Paisanita Petrovice": "Petrovice",
@@ -16,25 +15,41 @@ def process_foodora_data(download_folder):
 
     for file in files:
         df = pd.read_csv(file)
-        # df['Date'] = pd.to_datetime(df['Order Create Datetime'], format='%m/%d/%Y %H:%M').dt.date
         df['Date'] = pd.to_datetime(df['Order Create Datetime'], format='%Y-%m-%d %H:%M:%S').dt.date
         df['Location'] = df['Provider Name'].map(FOODORA_LOCATION_MAP)
         df['Provider'] = 'Foodora'
 
-        # Correct the item names and count the items
         df['Item Name'] = df['Item Name'].replace({
             'ChipÃ¡': 'Chipá',
             'ChampiÃ±ones': 'Champiñones',
             '30 ml': 'Chimichurri chico',
-            '12 pcs': 'Chipá'  # Assuming that '12 pcs' refers to double the count for Chipá
+            '12 pcs': 'Chipá'
         })
         df['Item Amount'] = df['Item Amount'].replace({'12 pcs': 2}).astype(int)
 
-        # Group by Date, Location, and Provider, and sum the Item Amount
         grouped_data = df.groupby(['Date', 'Location', 'Provider', 'Item Name'])['Item Amount'].sum().reset_index()
-        
-        all_data.append(grouped_data)
+
+        pivot_data = grouped_data.pivot_table(
+            index=['Date', 'Location', 'Provider'], 
+            columns='Item Name', 
+            values='Item Amount', 
+            fill_value=0
+        )
+        pivot_data.reset_index(inplace=True)
+
+        for product in PRODUCT_LIST:
+            if product not in pivot_data.columns:
+                pivot_data[product] = 0
+
+        # Convert all product columns to integers
+        pivot_data[PRODUCT_LIST] = pivot_data[PRODUCT_LIST].astype(int)
+
+        all_data.append(pivot_data)
 
     combined_data = pd.concat(all_data)
+
+    # Reorder columns to match PRODUCT_LIST
+    final_columns = ['Date', 'Location', 'Provider'] + PRODUCT_LIST
+    combined_data = combined_data[final_columns]
 
     return combined_data
--- main.py
+++ main.py
@@ -1,64 +1,48 @@
 import pandas as pd
-from constants import DOWNLOAD_FOLDER, PRODUCT_LIST
+import os
+import glob
+from constants import DOWNLOAD_FOLDER, PROVIDER_DICT
+
 from wolt_processor import process_wolt_data
 from bolt_processor import process_bolt_data
 from foodora_processor import process_foodora_data
-from output_processor import format_and_output
+
+def delete_provider_files(download_folder):
+    for pattern in PROVIDER_DICT.values():
+        for file in glob.glob(f"{download_folder}/{pattern}"):
+            os.remove(file)
+            print(f"Deleted file: {file}")
 
 def main():
-    # Process data from all providers
-    wolt_data = process_wolt_data(DOWNLOAD_FOLDER)
-    bolt_data = process_bolt_data(DOWNLOAD_FOLDER)
-    foodora_data = process_foodora_data(DOWNLOAD_FOLDER)
+    try:
+        # Process data from all providers
+        wolt_data = process_wolt_data(DOWNLOAD_FOLDER)
+        bolt_data = process_bolt_data(DOWNLOAD_FOLDER)
+        foodora_data = process_foodora_data(DOWNLOAD_FOLDER)
 
-    # Combine data from all providers
-    combined_data = pd.concat([wolt_data, bolt_data, foodora_data])
+        # Combine data from all providers
+        combined_data = pd.concat([wolt_data, bolt_data, foodora_data])
 
-    # Ensure all dates, providers, and locations are present
-    provider_order = ['Wolt', 'Bolt', 'Foodora']
-    location_order = ['Zizkov', 'Dejvice', 'Petrovice']
-    dates = combined_data['Date'].unique()
+        # Sort and process the combined data
+        combined_data.sort_values(by=['Date', 'Provider', 'Location'], inplace=True)
+        daily_summary = combined_data.groupby('Date').sum().reset_index()
+        daily_summary['Location'] = 'All Locations'
+        daily_summary['Provider'] = 'All Providers'
+        final_data = pd.concat([combined_data, daily_summary])
+        final_data.sort_values(by=['Date', 'Location', 'Provider'], inplace=True)
 
-    # Initialize an empty list to store the aggregated data frames
-    frames = []
+        # Format and output the final data
+        formatted_data = final_data.to_string(index=False)
+        print(formatted_data)
 
-    # Aggregate data for each date, provider, and location
-    for date in dates:
-        for provider in provider_order:
-            for location in location_order:
-                group_data = combined_data[
-                    (combined_data['Date'] == date) &
-                    (combined_data['Provider'] == provider) &
-                    (combined_data['Location'] == location)
-                ]
-                sum_data = group_data[PRODUCT_LIST].sum().astype(int).to_frame().T
-                sum_data['Date'] = date
-                sum_data['Location'] = location
-                sum_data['Provider'] = provider
-                frames.append(sum_data)
+        # Copy the formatted data to clipboard in a tab-delimited format
+        final_data.to_clipboard(index=False, header=True, sep='\t')
 
-    # Combine all frames
-    aggregated_data = pd.concat(frames, ignore_index=True)
+        # Delete the provider files after successful processing
+        delete_provider_files(DOWNLOAD_FOLDER)
 
-    # Calculate the summary line for each day and append
-    summary_data = combined_data.groupby('Date')[PRODUCT_LIST].sum().astype(int).reset_index()
-    summary_data['Location'] = 'All Locations'
-    summary_data['Provider'] = 'All Providers'
-    aggregated_data = pd.concat([aggregated_data, summary_data], ignore_index=True)
-
-    # Reorder columns to have 'Date', 'Location', 'Provider' at the beginning
-    col_order = ['Date', 'Location', 'Provider'] + PRODUCT_LIST
-    aggregated_data = aggregated_data[col_order]
-
-    # Sort the data
-    aggregated_data.sort_values(by=['Date', 'Provider', 'Location'], inplace=True)
-
-    # Format and output the final data
-    formatted_data = format_and_output(aggregated_data)
-
-    # Copy the formatted data to clipboard in a tab-delimited format
-    formatted_data.to_clipboard(index=False, header=True, sep='\t')
-    print(formatted_data)
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()

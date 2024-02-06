9d9ecc39ed94558cfb1e794e9e03b459e879f1c9
Muadiv <info@muadiv.com.ar>
2024-02-06 23:23:44 +0100

    fixed foodora error

--- foodora_processor.py
+++ foodora_processor.py
@@ -21,18 +21,23 @@ def process_foodora_data(download_folder):
             print(f"Skipping empty file: {file}")
             continue
         df["Order received at"] = df["Order received at"].astype(str)
-        df["Date"] = pd.to_datetime(df["Order received at"], format="%m-%d-%Y %H:%M", errors="coerce").dt.date
+        # df["Date"] = pd.to_datetime(df["Order received at"], format="%m-%d-%Y %H:%M", errors="coerce").dt.date
 
-        # df["Date"] = pd.to_datetime(df["Order received at"], format="%Y-%m-%d %H:%M").dt.date
+        df["Date"] = pd.to_datetime(df["Order received at"], format="%Y-%m-%d %H:%M").dt.date
         df["Location"] = df["Restaurant name"].map(FOODORA_LOCATION_MAP)
         df["Provider"] = "Foodora"
         item_counts = df["Order Items"].apply(process_items)
         df = pd.concat([df, item_counts], axis=1)
+
         df.dropna(subset=["Date"], inplace=True)
+
         df = df[["Date", "Location", "Provider"] + PRODUCT_LIST]
+
         df.fillna(0, inplace=True)
         all_data.append(df)
+
     combined_data = pd.concat(all_data).groupby(["Date", "Location", "Provider"]).sum().reset_index()
+
     return combined_data
 
 
@@ -41,6 +46,11 @@ def process_items(items_str):
     item_counts = {product: 0 for product in PRODUCT_LIST}
     for item in items:
         name = " ".join(word for word in item.split() if word.isalpha())
+        if " g" in name:
+            name = name[:-1]
+        name = name.rstrip()
+        if "Chimichurri" in name:
+            name = "Chimichurri chico"
         if name in PRODUCT_LIST:
             count = get_count(item, name)
             item_counts[name] += count
@@ -53,7 +63,7 @@ def get_count(item_str, product_name):
     if "Chipá x 6" in item_str:
         return 1 if product_name == "Chipá" else 0
     try:
-        count = int("".join(filter(str.isdigit, item_str.split("x")[0])))
+        count = int("".join(filter(str.isdigit, item_str.split("|")[0])))
     except ValueError:
         count = 0
     return count
